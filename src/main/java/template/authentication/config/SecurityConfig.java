dad6a94966b24b7c37307b69dc37ba7fa7437a31
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2023-08-16 15:26:30 +0300

    Initialize project

---
+++ src/main/java/template/authentication/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package template.authentication.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                    .requestMatchers("**").permitAll()
+                    .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
